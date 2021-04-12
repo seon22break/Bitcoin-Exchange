6e12e814407017ef8bdddc37922c0c43b24de693
Jhonatan Matías <36485771+seon22break@users.noreply.github.com>
2021-04-12 17:11:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
+import requests
+import json
+
 from ulauncher.api.client.Extension import Extension
 from ulauncher.api.client.EventListener import EventListener
 from ulauncher.api.shared.event import KeywordQueryEvent, ItemEnterEvent
 from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
 from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
 from ulauncher.api.shared.action.HideWindowAction import HideWindowAction
-import requests
-import json
+
 
 
 class DemoExtension(Extension):
